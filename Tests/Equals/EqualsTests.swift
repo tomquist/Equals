f1ed75bb01fa5882f4140d953cbec3f3ae1d3925
Tom Quist <tom@quist.de>
2016-03-25 16:32:37 +0100

    Added missing HashesTests to linux tests

--- Tests/Equals/EqualsTests.swift
+++ Tests/Equals/EqualsTests.swift
@@ -1,5 +1,5 @@
 import XCTest
-@testable import Equals
+import Equals
 
 private struct Person {
     let firstName: String?
--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -3,4 +3,5 @@ import XCTest
 
 XCTMain([
     EqualsTests(),
+    HashesTests(),
 ])
